321b11c82e2302cd90d25aa8c9d69fcde8716cdb
giulio <giuliop@gmail.com>
2012-01-22 22:48:55 +0100

    initial commit

---
+++ reload_nginx_conf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash 
+
+sudo nginx -t -c /etc/nginx/conf/nginx.conf
+echo "Do you wish to relaunch nginx?"
+select yn in "Yes" "No"; do
+        case $yn in
+           Yes ) sudo kill -HUP $( cat /var/run/nginx.pid ); break;;
+           No ) exit;;
+        esac
+done
