f884922beae3035b14b0a82bc471b20bceedee8f
giulio <giuliop@gmail.com>
2012-01-22 23:16:34 +0100

    create-gitconfig creates now the file with the hidden token

--- create-gitconfig.sh
+++ create-gitconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ~
-cp .gitconfig .gitconfig.backup
-sed 's/--hidden--/08d16bdfc1221e87df40dd20b8ff41f9/' ~/dev/dotfiles/.gitconfig-hidden-token > .gitconfig
+sed 's/08d16bdfc1221e87df40dd20b8ff41f9/--hidden--/' \
+    .gitconfig > ~/dev/dotfiles/.gitconfig-hidden-token 
 
